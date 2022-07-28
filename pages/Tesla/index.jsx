35bb21b33d8cd30db6e78df32441e0cba9ddff43
osamakhalid123 <osamakhalid797@gmail.com>
2022-07-28 22:24:53 +0200

    test pro

--- pages/Tesla/index.jsx
+++ pages/Tesla/index.jsx
@@ -2,34 +2,31 @@ import ReactPaginate from "react-paginate";
 import styles from "../../styles/News.module.css";
 import { useState } from "react";
 
-export const Index=({articles})=>{
+export const Index = ({ articles }) => {
   const [article, setarticle] = useState(articles?.slice(0, 100));
   const [pageNumber, setPageNumber] = useState(0);
 
   const articlePerPage = 10;
   const pagesVisited = pageNumber * articlePerPage;
 
-  const displayarticle = article?.slice(pagesVisited, pagesVisited + articlePerPage)
+  const displayarticle = article
+    ?.slice(pagesVisited, pagesVisited + articlePerPage)
     .map((news, index) => {
       return (
-        <div key={index} 
-        onClick={() => (window.open(`${news.url}`))}
-        style={
-          {backgroundImage: `url(${news.urlToImage})`,
-          backgroundPosition:"center",
-          backgroundSize: "cover",
-          
-        }} 
-        className={styles.news}>
-          
-
+        <div
+          key={index}
+          onClick={() => window.open(`${news.url}`)}
+          style={{
+            backgroundImage: `url(${news.urlToImage})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+          className={styles.news}
+        >
           <div className={styles.details}>
-             <h3 className={styles.title} >
-            {news.title}
-          </h3>
-          <p  className={styles.description}>{news.description}</p>
+            <h3 className={styles.title}>{news.title}</h3>
+            <p className={styles.description}>{news.description}</p>
           </div>
-
         </div>
       );
     });
@@ -40,42 +37,48 @@ export const Index=({articles})=>{
     setPageNumber(selected);
   };
 
- return(
-  <div>
-    <div className={styles.container}>
-  {displayarticle}
-  </div>
-<div className={styles.pagination}>
-
-      <ReactPaginate
-        previousLabel={"Previous"}
-        nextLabel={"Next"}
-        pageCount={pageCount}
-        onPageChange={changePage}
-        containerClassName={styles.paginationBttns}
-        previousLinkClassName={styles.previousBttn}
-        nextLinkClassName={styles.nextBttn}
-        disabledClassName={styles.paginationDisabled}
-        activeClassName={styles.paginationActive}
-      />
-</div>
-
-  </div>
-  
- )
-
- }
-
-
- export const getServerSideProps = async () => {
-  const apiResponse = await fetch(
-    `https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=${process.env.URL_KEY}`
+  return (
+    <div>
+      <div className={styles.container}>{displayarticle}</div>
+      <div className={styles.pagination}>
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={styles.paginationBttns}
+          previousLinkClassName={styles.previousBttn}
+          nextLinkClassName={styles.nextBttn}
+          disabledClassName={styles.paginationDisabled}
+          activeClassName={styles.paginationActive}
+        />
+      </div>
+    </div>
   );
-  const res = await apiResponse.json();
-  const { articles } = res;
-  return {
-    props: { articles },
-  };
+};
+
+export const getServerSideProps = async () => {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    // development build code
+    const apiResponse = await fetch(
+      `https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=${process.env.URL_KEY}`
+    );
+    const res = await apiResponse.json();
+    const { articles } = res;
+    return {
+      props: { articles },
+    };
+  } else {
+    // production build code
+    const apiResponse = await fetch(
+      `https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=35c3878043074acdbc44b69e156687db`
+    );
+    const res = await apiResponse.json();
+    const { articles } = res;
+    return {
+      props: { articles },
+    };
+  }
 };
 
 export default Index;
